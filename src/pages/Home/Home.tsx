b45e98f6fe27fa8b04f3575af2151b0c8785d08d
--delete <--delete>
2024-07-13 10:40:34 +0600

    add Advertisement section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Advertisement from "../../components/UI/Advertisement";
 import Banner from "../../components/UI/Banner";
 
 
@@ -6,7 +7,10 @@ import Banner from "../../components/UI/Banner";
 export default function Home() {
 
     return (
-        <Banner />
+        <>
+            <Banner />
+            <Advertisement />
+        </>
     )
 }
 
